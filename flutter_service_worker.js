4f296e45a105c3332e11c22d25c3596e99f8b01d
Pabak Dev <53967227+pabak-dev@users.noreply.github.com>
2025-03-07 04:57:24 +0600

    update mar 7

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "137ac1af178b636d8f32ee32b3320da0",
-"/": "137ac1af178b636d8f32ee32b3320da0",
-"main.dart.js": "107e77e973986c57cb0477eb567fd71c",
+"index.html": "b388111b28b198565fbe41b693d7124c",
+"/": "b388111b28b198565fbe41b693d7124c",
+"main.dart.js": "9479ee5a796703e8dce4563f37f8342a",
 "manifest.json": "273822b5b75f6d56a26cb50667a36a4f",
 "version.json": "965a96c24e9dbc68742cc1a78cd8665f"};
 // The application shell files that are downloaded before a service worker can
